81f730cb9c37c977d416e4e5e8c72961da2326ba
iamashwin99 <46030335+iamashwin99@users.noreply.github.com>
2019-03-16 12:10:02 +0530

    Update Rename_subtitles_for _easy_association.sh

--- Rename_subtitles_for _easy_association.sh
+++ Rename_subtitles_for _easy_association.sh
@@ -2,7 +2,10 @@ for file in *.vtt
 do
   
   v=${file::-10}
-  b=$(ls| grep "$v" |grep mp4)
-#  echo $b
-  mv "$b" "$(basename "$b" .mp4).vtt"
+  b=$(ls .. | grep "$v" |grep mp4)
+  c=${b::-2}
+  echo $b
+  echo $c
+  mv "$file" "$c"
+  mv "$c" "$(basename "$c" .m).vtt"
 done
