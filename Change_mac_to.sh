53c3de92135936319f90c94d89c4cdc40ea49538
iamashwin99 <46030335+iamashwin99@users.noreply.github.com>
2019-04-30 18:33:51 +0530

    Update Change_mac_to.sh

--- Change_mac_to.sh
+++ Change_mac_to.sh
@@ -1,4 +1,21 @@
 device=$(ip link show | grep wlp | cut -d" " -f2 | tr -d :)
+
+ip link show |tail |cat > 1.txt
+
+
+nmcli con down BPGC-WIFI
+
 ip link set dev $device down
+echo "Device is  down :" $device
+
 ip link set dev $device address $1
+echo "Device is  set :"
+
+ ip link show |tail |cat > 2.txt
+
+diff 1.txt 2.txt 
+
 ip link set dev $device up
+echo "Device is  up :" $device
+
+nmcli con up BPGC-WIFI
