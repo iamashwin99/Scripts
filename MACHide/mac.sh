507fe403ec65e5cd7de879b195102aacca37fc65
iamashwin99 <46030335+iamashwin99@users.noreply.github.com>
2019-09-27 20:32:29 +0530

    Update mac.sh

--- MACHide/mac.sh
+++ MACHide/mac.sh
@@ -4,7 +4,7 @@ device=$(ip link show | grep wlp | cut -d" " -f2 | tr -d :)
 ip link show |tail |cat > 1.txt
 
 
-nmcli con down BPGC-C_HOSTEL
+nmcli con down WIFI
 
 ip link set dev $device down
 echo "Device is  down :" $device
@@ -19,4 +19,4 @@ diff 1.txt 2.txt
 ip link set dev $device up
 echo "Device is  up :" $device
 
-nmcli con up BPGC-C_HOSTEL
+nmcli con up WIFI
